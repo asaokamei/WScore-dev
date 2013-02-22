780bd5086dc2279d80ea19efc768582d55188001
asao Kamei <asao@ubuPC.(none)>
2013-02-22 16:46:51 +0900

    added plain php file to show/clear apc cache

---
+++ public/cache.php
@@ -0,0 +1,15 @@
+<?php
+
+if( !function_exists( 'apc_clear_cache' ) ) {
+    echo 'apc not available.';
+}
+
+echo '<a href="index.php">index</a><br /> ';
+if( $_GET[ 'act' ] == 'cache-clear' ) {
+    apc_clear_cache( 'opcode' );
+    apc_clear_cache( 'user' );
+}
+echo '<a href="cache.php?act=cache-clear">clear cache</a>';
+$info =apc_cache_info( 'user' );
+unset( $info[ 'slot_distribution' ] );
+var_dump( $info );
