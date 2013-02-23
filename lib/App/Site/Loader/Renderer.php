489f97b33d2acb995019912d02b241a2950ff81f
asao Kamei <asao@ubuPC.(none)>
2013-02-23 16:28:24 +0900

    sets parent template if pathinfo under templates.

--- lib/App/Site/Loader/Renderer.php
+++ lib/App/Site/Loader/Renderer.php
@@ -6,7 +6,7 @@ class Renderer extends \WScore\Web\Loader\Renderer
     public function __construct()
     {
         $routes = array(
-            '/templates/*' => array( 'found' => 'true' ),
+            '/templates/*' => array( 'found' => 'true', 'parent' => 'template.php' ),
         );
         $this->setRoute( $routes );
     }
