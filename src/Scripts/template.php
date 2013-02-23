1e07dac7f9467b271c429af4f2f3525bb4640c56
asao Kamei <asao@ubuPC.(none)>
2013-02-23 16:12:57 +0900

    removed unused script.

--- src/Scripts/template.php
+++
@@ -1,6 +0,0 @@
-<?php
-use \App\App;
-$view = require( __DIR__ . '/../../vendor/wscore/template/scripts/instance.php' );
-$view->set( 'basePath', '/WSdev' ); // not good.
-$view->parent( \App\App::$template_root . '/layout.php' );
-return $view;
