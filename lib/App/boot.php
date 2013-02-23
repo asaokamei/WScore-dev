7dadcf1f2b19758e173a0ad3cb54a9c78771df7d
asao Kamei <asao@ubuPC.(none)>
2013-02-23 12:40:23 +0900

    use simple functions to boot $app.

---
+++ lib/App/boot.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * @return \App\App
+ */
+$boot = function()
+{
+    // set up folders.
+    $root = dirname( dirname( __DIR__ ) );
+    $template_root = $root . '/template';
+
+    /** @noinspection PhpIncludeInspection */
+    $service = include( $root . '/vendor/wscore/dicontainer/scripts/container.php' );
+    $service->set( 'ContainerInterface', $service );
+
+    // set Template
+    $service->set( 'Template', '\WScore\Template\Template', array(
+        'setter' => array(
+            'setRoot' => array( 'root' => $template_root ),
+            'parent' => array( 'parentTemplate' => 'layout.php' ),
+        )
+    ) );
+
+    // generate myself, app, object.
+    $app = $service->get( 'App\App' );
+    return $app;
+};
+
+
+/**
+ * @param string  $appName
+ * @param Closure $boot
+ * @return App\App
+ */
+function getApp( $appName, $boot )
+{
+    if( !function_exists( 'apc_fetch' ) ) return $boot();
+    if( $app = apc_fetch( $appName ) ) return $app;
+    $app = $boot();
+    apc_store( $appName, $app );
+    return $app;
+}
+
