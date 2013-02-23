ec47bea47ea7fdb38113c05fae5efc174c7f0fe7
asao Kamei <asao@ubuPC.(none)>
2013-02-23 12:41:06 +0900

    removed all but loader, which is what $app is for.

--- lib/App/App.php
+++ lib/App/App.php
@@ -13,47 +13,6 @@ use \WScore\DiContainer\ContainerInterface;
 
 class App extends \WScore\Web\FrontMC
 {
-    public static $appName = 'WsDemo';
-
-    /**
-     * @return App
-     */
-    public static function getCached() 
-    {
-        /** @var $app self */
-        if( !function_exists( 'apc_fetch' ) ) return self::start();
-        if( $app = apc_fetch( self::$appName ) ) return $app;
-        $app = self::start();
-        apc_store( self::$appName, $app );
-        return $app;
-    }
-
-    /**
-     * @return App
-     */
-    public static function start()
-    {
-        // set up folders.
-        $root = dirname( dirname( __DIR__ ) );
-        $template_root = $root . '/template';
-        
-        /** @noinspection PhpIncludeInspection */
-        $service = include( $root . '/vendor/wscore/dicontainer/scripts/container.php' );
-        $service->set( 'ContainerInterface', $service );
-
-        // set Template
-        $service->set( 'Template', '\WScore\Template\Template', array(
-            'setter' => array( 
-                'setRoot' => array( 'root' => $template_root ),
-                'parent' => array( 'parentTemplate' => 'layout.php' ),
-            )
-        ) );
-
-        // generate myself, app, object.
-        $app = $service->get( 'App\App' );
-        return $app;
-    }
-
     /**
      * @Inject
      * @param \App\Site\Loader\Renderer $render
