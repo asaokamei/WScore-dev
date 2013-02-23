2a6c6be829e7676006bc7b8c02def64d5e8460c6
asao Kamei <asao@ubuPC.(none)>
2013-02-23 14:20:47 +0900

    creates a password generator form.

--- lib/App/Site/Page/Password/Index.php
+++ lib/App/Site/Page/Password/Index.php
@@ -3,9 +3,32 @@ namespace App\Site\Page\Password;
 
 class Index
 {
+    /**
+     * @Inject
+     * @var \WScore\Html\Forms
+     */
+    protected $form;
+
     public function onGet( $match )
     {
+        $input = array( 'length' => 12, 'symbol' => false, 'count' => 5 );
+        $data = $this->form( $input );
         $data[ 'class' ]  = 'me:'.get_called_class();
+        return $data;
+    }
+
+    protected function form( $input )
+    {
+        $counts = array(
+            array(  '5', ' 5 passwords' ),
+            array( '10', '10 passwords' ),
+            array( '15', '15 passwords' ),
+        );
+        $data = array();
+        $data[ 'length' ]  = $this->form->input( 'range', 'length' ,$input[ 'length' ] )->min(5)->max(24);
+        $data[ 'symbol' ]  = $this->form->input( 'checkbox', 'symbol', 'checked' )->checked( $input[ 'symbol' ] );
+        $data[ 'count' ]   = $this->form->select( 'count', $counts, array( $input['count'] ) );
+
         return $data;
     }
 }
--- template/password/index.php
+++ template/password/index.php
@@ -4,16 +4,19 @@ $_v->parent( 'password.php' );
 ?>
 <h1>Generate Password</h1>
 <div class="row-fluid marketing">
-    <div class="span6">
-        <h3>tttt ttt</h3>
-        <p>aasdf asdf asdfasd fasdf asdf asfawea efasdfa sdfad fasdf asdfa sfasd fasd. </p>
-    </div>
-    <div class="span6">
-        <h3>more ttttt</h3>
-        <p>qwerqwer qwerqwerq werq werqwe rqwerqwerqwertqwerqwerqwe . </p>
-        <p>a l ljl ;lj lj;lkjkloiuo ij  ljl jl; j;lj lkj; kll;l . </p>
-    </div>
-    <div class="span12">
-        <p>12394123904712390 123 4129384 12394 12 4129038 41928074 0129 741209 412...</p>
-    </div>
+    <p>specify length of password, check to use symbols (!@#$ etc.), <br />and click generate password button. </p>
+    <form name="password" method="post" action="password.php">
+        <dl class="dl-horizontal">
+            <dt>length of password</dt>
+            <dd><?php echo $_v->get( 'length' )->class_( 'span4' ); ?>
+                &nbsp;<p class="muted">(minimum length of password is 5)</p>
+            </dd>
+            <dt>use symbols</dt>
+            <dd><label><?php echo $_v->get( 'symbol' ); ?> check if you want password to have some symbols. </label></dd>
+            <dt>get # of passwords</dt>
+            <dd><?php echo $_v->get( 'count' )->class_( 'span4' ); ?></dd>
+        </dl>
+        <input type="hidden" name="_act" value="generate">
+        <input type="submit" name="generate" class="btn btn-primary" value="generate password">
+    </form>
 </div>
