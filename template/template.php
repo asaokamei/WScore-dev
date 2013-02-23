6bbe61c70ab7a4f5b18333d42e3afd27f646b6be
asao Kamei <asao@ubuPC.(none)>
2013-02-23 16:29:06 +0900

    added a html sample file.

--- template/template.php
+++ template/template.php
@@ -8,6 +8,7 @@ $_v->parent( 'layout.php' );
 <ul class="nav nav-pills subMenu">
     <li><a href="index.php" >Top</a></li>
     <li><a href="another.php" >Another</a></li>
+    <li><a href="html.html" >Html</a></li>
 </ul>
 <h4>Template Folder</h4>
 <p>contents under template folder is rendered here. </p>
