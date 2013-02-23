4023e2afb94041326185f358437165fb25d21ff9
Asao <kamei@workspot.jp>
2013-02-23 10:34:28 +0900

    suppressed warning.

--- public/cache.php
+++ public/cache.php
@@ -5,11 +5,11 @@ if( !function_exists( 'apc_clear_cache' ) ) {
 }
 
 echo '<a href="index.php">index</a><br /> ';
-if( $_GET[ 'act' ] == 'cache-clear' ) {
+if( isset( $_GET[ 'act' ] ) && $_GET[ 'act' ] == 'cache-clear' ) {
     apc_clear_cache( 'opcode' );
     apc_clear_cache( 'user' );
 }
 echo '<a href="cache.php?act=cache-clear">clear cache</a>';
-$info =apc_cache_info( 'user' );
+$info = apc_cache_info( 'user' );
 unset( $info[ 'slot_distribution' ] );
 var_dump( $info );
