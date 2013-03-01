738e2a54a848ae742fea6687fee9f42b121fd510
asao Kamei <asao@ubuPC.(none)>
2013-03-01 10:17:54 +0900

    sub template: template.php is set in Renderer.

--- lib/App/Site/Loader/Renderer.php
+++ lib/App/Site/Loader/Renderer.php
@@ -6,7 +6,7 @@ class Renderer extends \WScore\Web\Loader\Renderer
     public function __construct()
     {
         $routes = array(
-            '/templates/*' => array( 'found' => 'true', 'parent' => 'template.php' ),
+            '/templates/*' => array( 'found' => 'true', 'addParent' => 'template.php' ),
             '/index.php'   => array( 'found' => 'true' ),
             '/'   => array( 'found' => 'true' ),
         );
