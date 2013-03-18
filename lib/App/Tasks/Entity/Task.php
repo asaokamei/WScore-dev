9206c89d31070dd2c20351a360152634c344ddc9
Asao <kamei@workspot.jp>
2013-03-18 20:48:20 +0900

    initial implementation of task entity and model.

---
+++ lib/App/Tasks/Entity/Task.php
@@ -0,0 +1,39 @@
+<?php
+namespace App\Tasks\Entity;
+
+use \WScore\DataMapper\Entity\EntityAbstract;
+
+class Task extends EntityAbstract
+{
+    const STATUS_ACTIVE = '1';
+    const STATUS_DONE   = '9';
+
+    protected $_model = 'task\model\tasks';
+
+    public $task_id = null;
+
+    public $task_memo = '';
+
+    public $task_date = '';
+
+    public $task_status = self::STATUS_ACTIVE;
+
+    public $new_dt_task;
+
+    public $mod_dt_task;
+
+    /**
+     * @return bool
+     */
+    public function isDone() {
+        return $this->task_status == self::STATUS_DONE;
+    }
+
+    /**
+     *
+     */
+    public function setDone() {
+        $this->task_status = self::STATUS_DONE;
+    }
+}
+
---
+++ lib/App/Tasks/Model/Tasks.php
@@ -0,0 +1,84 @@
+<?php
+namespace App\Tasks\Model;
+
+use \App\Tasks\Entity\Task;
+use \WScore\DataMapper\Model;
+
+class tasks extends Model
+{
+    /** @var string     name of database table     */
+    protected $table = 'demoTask';
+
+    /** @var string     name of primary key        */
+    protected $id_name = 'task_id';
+
+    protected $definition = array(
+        'task_id'     => array( 'task id',     'number', ),
+        'task_memo'   => array( 'what to do?', 'string', ),
+        'task_date'   => array( 'by when?',    'string', ),
+        'task_status' => array( 'done?',       'string', ),
+        'new_dt_task' => array( 'created at',  'string', 'created_at'),
+        'mod_dt_task' => array( 'updated at',  'string', 'updated_at'),
+    );
+
+    /** @var array      for validation of inputs       */
+    protected $validators = array(
+        'task_id'     => array( 'number' ),
+        'task_memo'   => array( 'text', 'required' ),
+        'task_date'   => array( 'date', '', ),
+        'task_status' => array( 'text', '' ),
+    );
+
+    /** @var array      for selector construction      */
+    protected $selectors  = array(
+        'task_id'     => array( 'Selector', 'text' ),
+        'task_memo'   => array( 'Selector', 'textarea', 'placeholder:your tasks here | class:span5 | rows:5' ),
+        'task_date'   => array( 'Selector', 'date', ),
+        'task_status' => array( 'Selector', 'checkToggle', '', array(
+            'items' => array( array( task::STATUS_ACTIVE, 'active' ), array( task::STATUS_DONE, 'done' ) )
+        ) ),
+    );
+
+    public $recordClassName = '\App\Tasks\Entity\Task';
+
+    // +----------------------------------------------------------------------+
+    /**
+     */
+    public function __construct()
+    {
+        
+    }
+
+    public function getCreateSql() {
+        $sql = "
+        CREATE TABLE {$this->table} (
+          task_id   INTEGER PRIMARY KEY AUTOINCREMENT,
+          task_memo text NOT NULL DEFAULT '',
+          task_date date,
+          task_status char(1) NOT NULL DEFAULT '1',
+          new_dt_task text,
+          mod_dt_task text
+        );
+        ";
+        return $sql;
+    }
+    public function getClearSql() {
+        $sql = "DROP TABLE IF EXISTS {$this->table}";
+        return $sql;
+    }
+    public function getSampleTasks( $idx=1 ) {
+        $memo = array(
+            1 => 'set done this task',
+            2 => 'modify this task',
+            3 => 'add a new task',
+            4 => 'try validation? set all blank and update/insert a task. ',
+            5 => 'delete all finished tasks and setup the task list',
+        );
+        $task = array(
+            'task_memo' => 'task #' . $idx . ' ' . $memo[ $idx ],
+            'task_status' => task::STATUS_ACTIVE,
+            'task_date' => sprintf( '2012-11-%02d', $idx + 1 ),
+        );
+        return $task;
+    }
+}
