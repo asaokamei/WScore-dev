4d78f2ea841dfca7527e6ecaed5a9b300169712f
Asao <kamei@workspot.jp>
2013-03-20 23:54:53 +0900

    shows some list of tasks.

--- lib/App/Tasks/Page/Index.php
+++ lib/App/Tasks/Page/Index.php
@@ -3,8 +3,32 @@ namespace App\Tasks\Page;
 
 class Index
 {
+    /**
+     * @Inject
+     * @var \WScore\DataMapper\EntityManager
+     */
+    protected $em;
+
+    /**
+     * @Inject
+     * @var \WScore\DataMapper\RoleManager
+     */
+    protected $role;
+    
+    /**
+     * @Inject
+     * @var \App\Tasks\Model\Tasks
+     */
+    protected $tasks;
+
     public function onGet( $match )
     {
-        return 'Tasks';
+        $tasks = $this->tasks->query()->select();
+        $tasks = $this->em->fetch( '\App\Tasks\Entity\Task', $tasks );
+        $roles = array();
+        foreach( $tasks as $key => $t ) {
+            $roles[$key] = $this->role->applyDataIO( $t );
+        }
+        return array( 'tasks' => $roles );
     }
 }
--- lib/App/Tasks/View/Index.php
+++ lib/App/Tasks/View/Index.php
@@ -1,7 +1,35 @@
+<h1>Task List</h1>
 <?php
 
 /** @var $this \WScore\Template\TemplateInterface */
 
+if( !$tasks = $this->get( 'tasks' ) ) {
+    echo '<p>not ready; more tasks to come... </p>';
+    return;
+}
 ?>
-<h1>Task List</h1>
-<p>not ready; more tasks to come... </p>
+<table class="table">
+    <thead>
+    <tr>
+        <th>task to do</th>
+        <th>done by</th>
+        <th>done</th>
+    </tr>
+    </thead>
+    <tbody>
+    <?php
+    foreach( $tasks as $task ) {
+        /** @var $task \WScore\DataMapper\Role\DataIO */
+        $task->setHtmlType( 'html' );
+        ?>
+        <tr>
+            <td><?php echo $task->popHtml( 'memo' ); ?></td>
+            <td><?php echo $task->popHtml( 'done_by' ); ?></td>
+            <td><?php echo $task->popHtml( 'status' ); ?></td>
+        </tr>
+    <?php
+    }
+    ?>
+    </tbody>
+</table>
+
