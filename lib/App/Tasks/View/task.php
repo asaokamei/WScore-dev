1a1b11f2557791b5bf724e57b90bc6c4567310c8
Asao <kamei@workspot.jp>
2013-03-20 19:32:44 +0900

    use appRoot.

--- lib/App/Tasks/View/task.php
+++ lib/App/Tasks/View/task.php
@@ -3,16 +3,16 @@
 /** @var $this \WScore\Template\TemplateInterface */
 $baseUrl = $this->get( 'baseUrl' );
 $taskUrl = $this->get( 'appUrl' );
-$appUrl  = $this->get( 'baseAppUrl' );
+$appRoot = $this->get( 'appRoot' );
 
 ?>
     <style type="text/css">
         ul.subMenu { clear: both; float: right;}
     </style>
     <ul class="nav nav-pills subMenu">
-        <li><a href="<?php echo $appUrl; ?>" >My Tasks</a></li>
-        <li><a href="<?php echo $appUrl; ?>create" >New Task</a></li>
-        <li><a href="<?php echo $appUrl; ?>setup" >setup</a></li>
+        <li><a href="<?php echo $appRoot; ?>" >My Tasks</a></li>
+        <li><a href="<?php echo $appRoot; ?>create" >New Task</a></li>
+        <li><a href="<?php echo $appRoot; ?>setup" >setup</a></li>
     </ul>
     <h4>task/todo demo</h4>
     <p>task/todo application using data mapper and basic MVC. </p>
