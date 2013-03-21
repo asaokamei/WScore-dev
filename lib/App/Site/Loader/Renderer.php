ba379a3072b07379082de2e8a29d8058a85cd3b8
asao Kamei <asao@ubuPC.(none)>
2013-03-21 12:37:35 +0900

    removed unused found match info.

--- lib/App/Site/Loader/Renderer.php
+++ lib/App/Site/Loader/Renderer.php
@@ -6,9 +6,9 @@ class Renderer extends \WScore\Web\Loader\Renderer
     public function __construct()
     {
         $routes = array(
-            '/templates/*' => array( 'found' => 'true', 'addParent' => 'template.php' ),
-            '/index.php'   => array( 'found' => 'true' ),
-            '/'   => array( 'found' => 'true', 'render' => 'index.php' ),
+            '/templates/*' => array( 'addParent' => 'template.php' ),
+            '/index.php'   => array(),
+            '/'   => array( 'render' => 'index.php' ),
         );
         $this->setRoute( $routes );
     }
