2d92943a3db39d705980591d9fa0ca4889c245a0
asao Kamei <asao@ubuPC.(none)>
2013-03-22 15:33:13 +0900

    show edit form for task.

--- lib/App/Tasks/Page/Edit.php
+++ lib/App/Tasks/Page/Edit.php
@@ -3,8 +3,36 @@ namespace App\Tasks\Page;
 
 class Edit
 {
+    /**
+     * @Inject
+     * @var \WScore\DataMapper\EntityManager
+     */
+    protected $em;
+
+    /**
+     * @Inject
+     * @var \WScore\DataMapper\RoleManager
+     */
+    protected $role;
+
+    /**
+     * @Inject
+     * @var \App\Tasks\Model\Tasks
+     */
+    protected $tasks;
+
+    /**
+     * @param array $match
+     * @return array
+     * @throws \Exception
+     */
     public function onGet( $match )
     {
+        $id = $match[ 'id' ];
+        //if( !$id ) throw new \Exception( 'no id', 404 );
+        $task = $this->em->fetch( 'App\Tasks\Entity\Task', $id );
+        $task = $this->role->applyDataIO( $task[0] );
+        $match[ 'task' ] = $task;
         return $match;
     }
 }
--- lib/App/Tasks/View/edit.php
+++ lib/App/Tasks/View/edit.php
@@ -4,4 +4,23 @@
 
 ?>
 <h1>Edit Task #<?php echo $this->id; ?></h1>
+<?php
+/** @var $task \WScore\DataMapper\Role\DataIO */
+$task = $this->get( 'task' );
+$task->setHtmlType( 'edit' );
+
+?>
+<form name="edit" action="" method="post">
+    <dl>
+        <dt>task memo</dt>
+        <dd><?php echo $task->popHtml( 'memo' )->class_( 'span5' ); ?></dd>
+        <dt>done by</dt>
+        <dd><?php echo $task->popHtml( 'done_by' )->class_( false ); ?></dd>
+        <dt>status</dt>
+        <dd><?php echo $task->popHtml( 'status' ); ?></dd>
+    </dl>
+    <input type="hidden" name="_method" value="put">
+    <button type="submit" class="btn btn-primary">save</button>
+    <a href="<?php echo $this->get( 'appRoot' ); ?>" class="btn">cancel</a>
+</form>
 <p>not ready; more tasks to come... </p>
