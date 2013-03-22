d48af3565568330c9ec479ca208594c728f8d66e
asao Kamei <asao@ubuPC.(none)>
2013-03-22 16:17:56 +0900

    added create/Create function.

--- lib/App/Tasks/Page/Create.php
+++ lib/App/Tasks/Page/Create.php
@@ -3,8 +3,54 @@ namespace App\Tasks\Page;
 
 class Create
 {
+    /**
+     * @Inject
+     * @var \WScore\DataMapper\EntityManager
+     */
+    protected $em;
+
+    /**
+     * @Inject
+     * @var \WScore\DataMapper\RoleManager
+     */
+    protected $role;
+
+    /**
+     * @Inject
+     * @var \App\Tasks\Model\Tasks
+     */
+    protected $tasks;
+
+    /**
+     * @param array $match
+     * @return array
+     * @throws \Exception
+     */
     public function onGet( $match )
     {
-        return 'Create';
+        $task = $this->em->newEntity( '\App\Tasks\Entity\Task' );
+        $task = $this->role->applyDataIO( $task );
+        $match[ 'task' ] = $task;
+        return $match;
+    }
+
+    /**
+     * @param array $match
+     * @return array
+     * @throws \Exception
+     */
+    public function onPost( $match )
+    {
+        $task = $this->em->newEntity( '\App\Tasks\Entity\Task' );
+        $task = $this->role->applyDataIO( $task );
+        $task->load( $_POST );
+        if( $task->validate() ) {
+            $active = $this->role->applyActive( $task );
+            $active->save();
+            header( "Location: " . $match[ 'appRoot' ] );
+            exit;
+        }
+        $match[ 'task' ] = $task;
+        return $match;
     }
 }
--- lib/App/Tasks/View/create.php
+++ lib/App/Tasks/View/create.php
@@ -2,6 +2,44 @@
 
 /** @var $this \WScore\Template\TemplateInterface */
 
+/** @var $task \WScore\DataMapper\Role\DataIO */
+$task = $this->get( 'task' );
+$task->setHtmlType( 'edit' );
+
+if( $this->get( 'onMethod' ) == 'onPost' ) {
+    $title = 'Re-Enter New Task';
+} else {
+    $title = 'New Task';
+}
+
+$view = $this;
+/**
+ * @param \WScore\DataMapper\Role\DataIO $task
+ * @param string $key
+ * @return string
+ */
+function error( $task, $key ) {
+    if( !$task->isError( $key ) ) return '';
+    return '<br /><p class="text-error">'. $task->getError( $key ) . '</p>';
+}
+
 ?>
-<h1>New Task</h1>
-<p>not ready; more tasks to come... </p>
+<h1><?php echo $title; ?></h1>
+<form name="edit" action="" method="post">
+    <dl>
+        <dt>task memo</dt>
+        <dd><?php echo $task->popHtml( 'memo' )->class_( 'span5' ); ?>
+            <?php echo error( $task, 'memo' ); ?>
+        </dd>
+        <dt>done by</dt>
+        <dd><?php echo $task->popHtml( 'done_by' )->class_( false ); ?>
+            <?php echo error( $task, 'done_by' ); ?>
+        </dd>
+        <dt>status</dt>
+        <dd><?php echo $task->popHtml( 'status' ); ?>
+            <?php echo error( $task, 'status' ); ?>
+        </dd>
+    </dl>
+    <button type="submit" class="btn btn-primary">save</button>
+    <a href="<?php echo $this->get( 'appRoot' ); ?>" class="btn">cancel</a>
+</form>
