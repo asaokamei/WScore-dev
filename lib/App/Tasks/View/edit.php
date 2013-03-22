f9014323994555b0b01fd62031ac7e3167ea1c70
asao Kamei <asao@ubuPC.(none)>
2013-03-22 16:04:13 +0900

    show error message if in validated.

--- lib/App/Tasks/View/edit.php
+++ lib/App/Tasks/View/edit.php
@@ -3,21 +3,43 @@
 /** @var $this \WScore\Template\TemplateInterface */
 
 ?>
-<h1>Edit Task #<?php echo $this->id; ?></h1>
 <?php
 /** @var $task \WScore\DataMapper\Role\DataIO */
 $task = $this->get( 'task' );
 $task->setHtmlType( 'edit' );
 
+if( $this->get( 'onMethod' ) == 'onPut' ) {
+    $title = 'Re-Edit Task #';
+} else {
+    $title = 'Edit Task #';
+}
+
+$view = $this;
+/**
+ * @param \WScore\DataMapper\Role\DataIO $task
+ * @param string $key
+ * @return string
+ */
+function error( $task, $key ) {
+    if( !$task->isError( $key ) ) return '';
+    return '<br />'. $task->getError( $key );
+}
 ?>
+<h1><?php echo $title, $this->id; ?></h1>
 <form name="edit" action="" method="post">
     <dl>
         <dt>task memo</dt>
-        <dd><?php echo $task->popHtml( 'memo' )->class_( 'span5' ); ?></dd>
+        <dd><?php echo $task->popHtml( 'memo' )->class_( 'span5' ); ?>
+        <?php echo error( $task, 'memo' ); ?>
+        </dd>
         <dt>done by</dt>
-        <dd><?php echo $task->popHtml( 'done_by' )->class_( false ); ?></dd>
+        <dd><?php echo $task->popHtml( 'done_by' )->class_( false ); ?>
+            <?php echo error( $task, 'done_by' ); ?>
+        </dd>
         <dt>status</dt>
-        <dd><?php echo $task->popHtml( 'status' ); ?></dd>
+        <dd><?php echo $task->popHtml( 'status' ); ?>
+            <?php echo error( $task, 'status' ); ?>
+        </dd>
     </dl>
     <input type="hidden" name="_method" value="put">
     <button type="submit" class="btn btn-primary">save</button>
