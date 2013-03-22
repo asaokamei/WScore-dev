0b81c2b20760e5e5dd3fb19e5c68009e037edb11
asao Kamei <asao@ubuPC.(none)>
2013-03-22 15:47:14 +0900

    implemented edit::onPut. just saves nothing more.

--- lib/App/Tasks/Page/Edit.php
+++ lib/App/Tasks/Page/Edit.php
@@ -28,11 +28,43 @@ class Edit
      */
     public function onGet( $match )
     {
-        $id = $match[ 'id' ];
-        //if( !$id ) throw new \Exception( 'no id', 404 );
-        $task = $this->em->fetch( 'App\Tasks\Entity\Task', $id );
-        $task = $this->role->applyDataIO( $task[0] );
+        $task = $this->fetchTask( $match );
+        $task = $this->role->applyDataIO( $task );
         $match[ 'task' ] = $task;
         return $match;
+    }
+
+
+    /**
+     * @param array $match
+     * @return array
+     * @throws \Exception
+     */
+    public function onPut( $match )
+    {
+        $task = $this->fetchTask( $match );
+        $task = $this->role->applyDataIO( $task );
+        $task->load( $_POST );
+        if( $task->validate() ) {
+            $active = $this->role->applyActive( $task );
+            $active->save();
+        }
+
+        $match[ 'task' ] = $task;
+        return $match;
+    }
+
+    /**
+     * @param $match
+     * @return mixed
+     * @throws \Exception
+     */
+    private function fetchTask( $match )
+    {
+        if( !isset( $match[ 'id' ] ) ) throw new \Exception( 'task id not set', 1400 );
+        $id = $match[ 'id' ];
+        $task = $this->em->fetch( 'App\Tasks\Entity\Task', $id );
+        if( empty( $task ) ) throw new \Exception( 'task not found: id='.$id, 1401 );
+        return $task[0];
     }
 }
