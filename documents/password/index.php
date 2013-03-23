ddb43355013e1ff56127b1bdd9ca191e708834f7
Asao <kamei@workspot.jp>
2013-03-23 12:42:05 +0900

    added test for password generation.

--- documents/password/index.php
+++ documents/password/index.php
@@ -3,6 +3,7 @@
 $this->parent( 'password.php' );
 ?>
 <h1>Generate Password</h1>
+<!-- Template: documents/password/index -->
 <div class="row-fluid marketing">
     <p>specify length of password, check to use symbols (!@#$ etc.), <br/>and click generate password button. </p>
 
---
+++ lib/test/App/App_PasswordTest.php
@@ -0,0 +1,74 @@
+<?php
+namespace test\App;
+
+use \WSTests\DataMapper\entities\friend;
+
+require( __DIR__ . '/../../bootstrap.php' );
+
+class App_PasswordTests extends \PHPUnit_Framework_TestCase
+{
+    /** @var \App\App */
+    public $app;
+
+    public $template_root;
+
+    /**
+     *
+     */
+    function setUp()
+    {
+        /** @var $container \App\App */
+        $this->app = \App\getApp( 'WsTest-app', false );
+        $this->template_root = __DIR__ . '/../../../documents/';
+    }
+
+    function test_password_menu()
+    {
+        $server = array(
+            'REQUEST_METHOD' => 'get',
+            'SCRIPT_NAME'    => '/test/app.php',
+            'REQUEST_URI'    => '/test/password/index.php',
+        );
+        $this->app->pathInfo( $server );
+        /** @var $response \WScore\Web\Http\Response */
+        $response = $this->app->run();
+        $contents = $response->content;
+
+        // read index.php
+        $this->assertContains( '<form name="password" method="post" action="index.php">', $contents );
+        $this->assertContains( '<!-- Template: documents/password -->', $contents );
+        $this->assertContains( '<!-- Template: documents/password/index -->', $contents );
+        $this->assertContains( '<dt>length of password</dt>', $contents );
+    }
+
+    function test_password_post()
+    {
+        $server = array(
+            'REQUEST_METHOD' => 'POST',
+            'SCRIPT_NAME'    => '/test/app.php',
+            'REQUEST_URI'    => '/test/password/index.php',
+        );
+        $post = array(
+            'range' => '12',
+            'count' => '6',
+            'symbol' => '',
+        );
+        $this->app->pathInfo( $server );
+        /** @var $response \WScore\Web\Http\Response */
+        $response = $this->app->using( $post )->run();
+        $contents = $response->content;
+
+        // read index.php
+        $this->assertContains( '<form name="password" method="post" action="index.php">', $contents );
+        $this->assertContains( '<!-- Template: documents/password -->', $contents );
+        $this->assertContains( '<!-- Template: documents/password/index -->', $contents );
+        $this->assertContains( '<dt>length of password</dt>', $contents );
+        $this->assertContains( '            <table class="table">
+            <tr>
+                <th>password</th>
+                <th>crypt</th>
+                <th>md5</th>
+            </tr>', $contents );
+    }
+    
+}
