f1c685f7343b8b68c069d32f78c3215251c46706
Asao <kamei@workspot.jp>
2013-03-23 17:37:17 +0900

    cannot redeclare function in series of tests. use closure.

--- lib/App/Tasks/View/create.php
+++ lib/App/Tasks/View/create.php
@@ -18,7 +18,7 @@ $view = $this;
  * @param string $key
  * @return string
  */
-function error( $task, $key ) {
+$error = function( $task, $key ) {
     if( !$task->isError( $key ) ) return '';
     return '<br /><p class="text-error">'. $task->getError( $key ) . '</p>';
 }
@@ -30,15 +30,15 @@ function error( $task, $key ) {
     <dl>
         <dt>task memo</dt>
         <dd><?php echo $task->popHtml( 'memo' )->class_( 'span5' ); ?>
-            <?php echo error( $task, 'memo' ); ?>
+            <?php echo $error( $task, 'memo' ); ?>
         </dd>
         <dt>done by</dt>
         <dd><?php echo $task->popHtml( 'done_by' )->class_( false ); ?>
-            <?php echo error( $task, 'done_by' ); ?>
+            <?php echo $error( $task, 'done_by' ); ?>
         </dd>
         <dt>status</dt>
         <dd><?php echo $task->popHtml( 'status' ); ?>
-            <?php echo error( $task, 'status' ); ?>
+            <?php echo $error( $task, 'status' ); ?>
         </dd>
     </dl>
     <button type="submit" class="btn btn-primary">save</button>
