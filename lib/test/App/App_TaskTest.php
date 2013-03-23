9f3fc248759b30cc7d8051339681575909519cfc
Asao <kamei@workspot.jp>
2013-03-23 17:37:48 +0900

    test if each url returns template file with right needle.

--- lib/test/App/App_TaskTest.php
+++ lib/test/App/App_TaskTest.php
@@ -5,12 +5,14 @@ use \WSTests\DataMapper\entities\friend;
 
 require( __DIR__ . '/../../bootstrap.php' );
 
-class App_PwdTests extends \PHPUnit_Framework_TestCase
+class App_TaskTests extends \PHPUnit_Framework_TestCase
 {
     /** @var \App\App */
     public $app;
 
     public $template_root;
+    
+    public $document_root;
 
     /**
      *
@@ -19,9 +21,95 @@ class App_PwdTests extends \PHPUnit_Framework_TestCase
     {
         /** @var $container \App\App */
         $this->app = \App\getApp( 'WsTest-app', false );
+        $this->document_root = __DIR__ . '/../../../documents/';
         $this->template_root = __DIR__ . '/../../../lib/App/Tasks/View/';
     }
 
+    public function test_task_needles()
+    {
+        $this->verifyNeedles(
+            'tasks/',
+            array(
+                $this->document_root . 'layout.php',
+                $this->template_root . 'task.php',
+                $this->template_root . 'index.php'
+            )
+        );
+    }
+    function test_create_needles()
+    {
+        $this->verifyNeedles(
+            'tasks/create',
+            array(
+                $this->document_root . 'layout.php',
+                $this->template_root . 'task.php',
+                $this->template_root . 'create.php'
+            )
+        );
+    }
+    function test_setup_needles()
+    {
+        $this->verifyNeedles(
+            'tasks/setup',
+            array(
+                $this->document_root . 'layout.php',
+                $this->template_root . 'task.php',
+                $this->template_root . 'setup.php'
+            )
+        );
+    }
+    function test_edit_needles()
+    {
+        $this->verifyNeedles(
+            'tasks/1',
+            array(
+                $this->document_root . 'layout.php',
+                $this->template_root . 'task.php',
+                $this->template_root . 'edit.php'
+            )
+        );
+    }
+    
+    public function verifyNeedles( $page, $sources )
+    {
+        $contents = $this->getAppContents( $page );
+        foreach( $sources as $src ) {
+            $needle = $this->getFileContents( $src );
+            $needle = $this->extractHtmlTestNeedles( $needle );
+            $this->assertContains( $needle, $contents );
+        } 
+    }
+    /**
+     * @param string $file
+     * @return string
+     */
+    public function getFileContents( $file )
+    {
+        return file_get_contents( $file );
+    }
+    /**
+     * @param array|string $server
+     * @return string
+     */
+    public function getAppContents( $server ) 
+    {
+        if( !is_array( $server ) ) {
+            $server = array(
+                'REQUEST_METHOD' => 'GET',
+                'SCRIPT_NAME'    => '/test/app.php',
+                'REQUEST_URI'    => '/test/' . $server,
+            );
+        }
+        $this->app->pathInfo( $server );
+        /** @var $response \WScore\Web\Http\Response */
+        $response = $this->app->run();
+        return $response->content;
+    }
+
+    /**
+     * @param string $html
+     * @return string
+     */
     public function extractHtmlTestMatches( $html )
     {
         $startTag = '<!-- HtmlTest: matchStart -->';
@@ -31,13 +119,17 @@ class App_PwdTests extends \PHPUnit_Framework_TestCase
         }
         return $html;
     }
-    
+
+    /**
+     * @param string $html
+     * @return array
+     */
     public function extractHtmlTestNeedles( $html )
     {
         $needles = array();
         $startTag = '<!-- HtmlTest: Needle=';
         $endTag   = ' -->';
-        if( preg_match_all( "/({$startTag}.*?{$endTag})/ms", $html, $match ) ) {
+        if( preg_match( "/({$startTag}.*?{$endTag})/ms", $html, $match ) ) {
             $needles = $match[1];
         }
         return $needles;
