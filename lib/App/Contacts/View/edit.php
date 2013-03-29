bf3f781a65b73f7da1773a0c48cdeb26af572a3c
Asao <kamei@workspot.jp>
2013-03-30 08:58:06 +0900

    added edit button; also put within if statement (instead of return) to show the button.

--- lib/App/Contacts/View/edit.php
+++ lib/App/Contacts/View/edit.php
@@ -29,22 +29,29 @@ $includeForm = '/edit-' . $method . '.php';
     $contacts = $this->get( 'contacts' );
     if( !$contacts || empty( $contacts ) ) {
         echo '<dt>no contacts yet...</dt>';
-        return;
     }
-    $selType  = $contacts[0]->form( 'type' );
-    $cByType  = array();
-    foreach( $contacts as $contact ) {
-        /** @var $cont \App\Contacts\Entity\Contact */
-        $cont = $contact->retrieve();
-        $type = $cont->type;
-        $cByType[ $type ][] = $contact;
-    }
-    foreach( $cByType as $byType => $contacts ) {
-        $name = $selType->popHtml( 'html', $byType );
-        echo "<dt>{$name}</dt>";
+    else {
+        
+        $selType  = $contacts[0]->form( 'type' );
+        $cByType  = array();
         foreach( $contacts as $contact ) {
-            echo '<dd>'.$contact->popHtml( 'info', 'html' ).'</dd>';
+            /** @var $cont \App\Contacts\Entity\Contact */
+            $cont = $contact->retrieve();
+            $type = $cont->type;
+            $cByType[ $type ][] = $contact;
+        }
+        foreach( $cByType as $byType => $contacts ) {
+            $name = $selType->popHtml( 'html', $byType );
+            echo "<dt>{$name}</dt>";
+            foreach( $contacts as $contact ) {
+                echo '<dd>'.$contact->popHtml( 'info', 'html' ).'</dd>';
+            }
         }
     }
     ?>
 </dl>
+<form name="edit" method="post" action="">
+    <input type="hidden" name="_method" value="edit" />
+    <button type="submit" class="btn btn-primary">edit contact</button>
+    <a href="<?php echo $this->get( 'appRoot' ); ?>" class="btn">back to list</a>
+</form> 
