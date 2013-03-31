a18d391503dbc1faea1e7da7367f36f678da722b
Asao <kamei@workspot.jp>
2013-03-31 17:41:31 +0900

    removed interaction menu, put database demo together, style change.

--- documents/index.php
+++ documents/index.php
@@ -7,7 +7,7 @@ $this->HomePage = true;
 
             /* Main marketing message and sign up button */
         .jumbotron {
-            margin: 60px 0;
+            margin: 30px 0;
             text-align: center;
         }
         .jumbotron h1 {
@@ -37,19 +37,13 @@ $this->HomePage = true;
             <h4>&gt; <a href="password/index.php" >generate password</a></h4>
             <p>demo for building form elements. 
                 another demo using AppLoader (<a href="pwd/" >pwd</a>). </p>
-            <h4>&gt; <a href="tasks/" >task/todo application</a></h4>
-            <p>demo for using data mapper, model, and entity. </p>
         </div>
         <div class="span6">
-            <h3>Experimental Demo</h3>
+            <h3>DataBase Sample</h3>
+            <h4>&gt; <a href="tasks/" >task/todo application</a></h4>
+            <p>demo for using data mapper, model, and entity. </p>
             <h4>&gt; <a href="contacts/" >contacts using Cena</a></h4>
             <p>a conceptual demo for using Cena-DTA using friends and contact demo. </p>
-            <h4>&gt; interaction with forms</h4>
-            <p>demo for data mapper and interactions. </p>
-            <ul>
-                <li><a href="interaction1.php" >demo #1: insert friend's data</a></li>
-                <li><a href="interaction2.php" >demo #2: interaction like a wizard</a></li>
-            </ul>
             <h3>Caching Application</h3>
             <p>development still undergoing. more demo should come up, hopefully sometime soon...</p>
             <p>[<a href="info.php">php info</a>],
