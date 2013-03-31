6a781c6954297a5f988b93b5a382c89c262bbe82
Asao <kamei@workspot.jp>
2013-03-31 21:07:06 +0900

    set up the logger (Monolog).

--- lib/App/boot.php
+++ lib/App/boot.php
@@ -11,6 +11,7 @@ function buildApp( $cache )
     $root = dirname( dirname( __DIR__ ) );
     $template_root = $root . '/documents';
 
+    // set up container/service. 
     /** @noinspection PhpIncludeInspection */
     if( $cache ) {
         $service = include( $root . '/vendor/wscore/dicontainer/scripts/withCache.php' );
@@ -18,9 +19,18 @@ function buildApp( $cache )
         $service = include( $root . '/vendor/wscore/dicontainer/scripts/instance.php' );
     }
     $service->set( 'ContainerInterface', $service );
+    
+    // set up database access
     $service->set( '\Pdo', 'dsn=mysql:dbname=test_WScore username=admin password=admin' );
+    
+    // set up logger
+    $service->set( 'LoggerInterface', '\Monolog\Logger', array( 'singleton' => true ) );
+    /** @var $logger \Monolog\Logger */
+    $logger = $service->get( 'LoggerInterface' );
+    $logger->pushHandler( new \Monolog\Handler\ChromePHPHandler() );
+    $logger->addInfo( 'starting WScore demo' );
 
-    // set Template
+    // set up Template
     $service->set( 'TemplateRoot',      $template_root );
     $service->set( 'TemplateLayout',    'layout.php' );
     $service->set( 'TemplateInterface', '\WScore\Template\PhpTemplate', array(
