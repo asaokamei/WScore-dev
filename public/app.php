70b8cc7ef2c905cfa7ff665434f9eedf49ab989d
asao Kamei <asao@ubuPC.(none)>
2013-04-01 16:00:08 +0900

    log when critical exception is caught.

--- public/app.php
+++ public/app.php
@@ -15,8 +15,15 @@ try {
     echo $app->template->setTemplate( 'errors/e404.php' )->render();
 
 } catch ( Exception $e ) {
+
     $code = $e->getCode();
     $app->template->set( 'code', $code );
-    if( !in_array( $code, array( '400', '404' ) ) ) $code = '503';
+    if( !in_array( $code, array( '400', '404' ) ) ) {
+        // last minute log about the crash.
+        if( isset( $app ) && isset( $app->logger ) ) {
+            $app->logger->critical( 'crashed', array( 'exception' => $e ) );
+        }
+        $code = '503';
+    }
     echo $app->template->setTemplate( "errors/e{$code}.php" )->render();
 }
