ebd8b4252ca2c9801b687de2e140694fc35c6ed2
asao Kamei <asao@ubuPC.(none)>
2013-04-03 15:49:46 +0900

    phpinfo as is.

--- public/info.php
+++ public/info.php
@@ -1,9 +1,3 @@
 <?php
-require_once( __DIR__ . '/../lib/bootstrap.php' );
-$app = App\getApp( 'info', false );
-
-    /** @var $view WScore\Template\TemplateInterface */
-$view = $app->container->get( 'TemplateInterface' );
-$view->renderSelf();
 
 phpinfo();
