84949089e1f6222b93a983a4f058f358ddac6559
Asao <kamei@workspot.jp>
2013-04-06 17:05:45 +0900

    hack for cli-server. un-psr-2.

--- public/app.php
+++ public/app.php
@@ -2,11 +2,13 @@
 require_once( __DIR__ . '/../app/bootstrap.php' );
 
 // router.php
-if (php_sapi_name() == 'cli-server') {
+if( php_sapi_name() == 'cli-server' ) {
     /* route static assets and return false */
-    if (preg_match('/\.(?:png|jpg|jpeg|gif|css|js)$/', $_SERVER["REQUEST_URI"])) {
+    if( preg_match('/\.(?:png|jpg|jpeg|gif|css|js)$/', $_SERVER["REQUEST_URI"] ) ) {
         return false;    // serve the requested resource as-is.
     }
+    // hack.
+    $_SERVER[ 'SCRIPT_NAME' ] = '/';
 }
 
 try {
