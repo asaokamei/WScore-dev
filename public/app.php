9c491bbb5e09501f87acc8632cd9575adc634a89
Asao <kamei@workspot.jp>
2013-04-06 14:23:38 +0900

    moved {get|build}App inside try-catch; prepared for sapi/server.

--- public/app.php
+++ public/app.php
@@ -1,11 +1,22 @@
 <?php
 require_once( __DIR__ . '/../app/bootstrap.php' );
-/** @var $app App\App */
-$app = App\getApp( 'WsDemo-app', true );
-$app = App\debugApp( $app );
+
+// router.php
+if (php_sapi_name() == 'cli-server') {
+    /* route static assets and return false */
+    if (preg_match('/\.(?:png|jpg|jpeg|gif|css|js)$/', $_SERVER["REQUEST_URI"])) {
+        return false;    // serve the requested resource as-is.
+    }
+}
 
 try {
 
+    // make app object.
+    /** @var $app App\App */
+    $app = App\getApp( 'WsDemo-app', true );
+    $app = App\debugApp( $app );
+
+    // set app for serving web. 
     $app->pathInfo( $_SERVER );
     $app->logger->info( 'app->run', array( 'time' => date( 'Y-m-d H:i:s' ), 'path' => $app->pathInfo ) );
     $response = $app->run();
