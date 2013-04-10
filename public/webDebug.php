8f8d9e22730603159f9c09edd57b2bbff712170f
Asao <kamei@workspot.jp>
2013-04-10 23:33:57 +0900

    made {apc|cache}.php accessible via cli-server, but not working cos apc are not present in cli-server.

--- public/webDebug.php
+++ public/webDebug.php
@@ -11,12 +11,16 @@
 
 // router.php
 if( php_sapi_name() == 'cli-server' ) {
+    $uri = $_SERVER["REQUEST_URI"];
     /* route static assets and return false */
     if( preg_match('/\.(?:png|jpg|jpeg|gif|css|js)$/', $_SERVER["REQUEST_URI"] ) ) {
         return false;    // serve the requested resource as-is.
     }
+    if( in_array( $_SERVER['REQUEST_URI'], array( '/apc.php', '/cache.php' ) ) ) {
+        return false;
+    }
     // hack.
-    $_SERVER[ 'SCRIPT_NAME' ] = '/app.php';
+    $_SERVER[ 'SCRIPT_NAME' ] = '/web.php';
 }
 
 $runMode = 'debug';
