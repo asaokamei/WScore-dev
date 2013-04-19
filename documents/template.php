719dec11d2f37c31ed477f08d88e317da1fc0b6c
Asao <kamei@workspot.jp>
2013-04-20 08:06:53 +0900

    removed file extensions from href.

--- documents/template.php
+++ documents/template.php
@@ -6,9 +6,9 @@
 </style>
 <!-- HtmlTest: Needle=documents/template -->
 <ul class="nav nav-pills subMenu">
-    <li><a href="index.php" >Top</a></li>
-    <li><a href="another.php" >Another</a></li>
-    <li><a href="html.html" >Html</a></li>
+    <li><a href="index" >Top</a></li>
+    <li><a href="another" >Another</a></li>
+    <li><a href="html" >Html</a></li>
 </ul>
 <h4>documents</h4>
 <p>some texts about WScore Framework. </p>
