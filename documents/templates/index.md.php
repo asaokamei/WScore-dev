0c355d789f7d3253b5a808ace2867ffeb270a99e
Asao <kamei@workspot.jp>
2013-04-20 16:27:07 +0900

    moved LICNESE.txt under top folder; copied it in the document file.

--- documents/templates/index.md.php
+++ documents/templates/index.md.php
@@ -1,4 +1,6 @@
-<h1>WScore Framework</h1>
+WScore Framework
+================
+
 <h3>yet another framework for PHP...</h3>
 <p>It's a PHP framework that is simple and small code base. </p>
 <p>But it has a unique technology called <a href="cena" >Cena Data Transfer Agent (Cena-DTA)</a>. </p>
@@ -7,4 +9,4 @@ known as cloud and application. With Cena DTA, you also can create complex html 
 entities and relations, and reflect the changes into database with 
 a very little/simple code. </p>
 <h3>license</h3>
-<p><a href="../LICENSE" >GPL version 2.</a></p>
+<p><a href="LICENSE" >GPL version 2.</a></p>
