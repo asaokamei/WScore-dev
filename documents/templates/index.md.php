3401d96391c73b61a8780388f7dafde4112b235b
Asao <kamei@workspot.jp>
2013-04-20 16:28:02 +0900

    write more text.

--- documents/templates/index.md.php
+++ documents/templates/index.md.php
@@ -1,12 +1,72 @@
 WScore Framework
 ================
 
-<h3>yet another framework for PHP...</h3>
-<p>It's a PHP framework that is simple and small code base. </p>
-<p>But it has a unique technology called <a href="cena" >Cena Data Transfer Agent (Cena-DTA)</a>. </p>
-<p>Cena-DTA allows to synchronize databases between client and server, or more recently 
-known as cloud and application. With Cena DTA, you also can create complex html forms with many 
-entities and relations, and reflect the changes into database with 
-a very little/simple code. </p>
-<h3>license</h3>
-<p><a href="LICENSE" >GPL version 2.</a></p>
+###yet another framework for PHP...
+
+It's a PHP framework that is simple and small code base. 
+
+But it has a unique technology called [Cena Data Transfer Agent (Cena-DTA)](cena).
+
+Cena-DTA allows to synchronize databases between client and server, or more recently 
+known as cloud and application. With Cena DTA, you also can create complex html forms 
+that have many entities and relations, and saves them into database with a little code.
+
+
+###serving files
+
+WScore can serve simple text files, such as html, markdown, or text. 
+The contntes are automatically converted to html based on the file extension, 
+as follows. 
+
+.md, .markdown, and .md.php 
+: markdown file.
+: contents are convereted to html using PHP-markdown-extra. 
+
+.txt, .text, and .txt.php
+: text file.
+: contents are rendered as pre-formated text using pre tag. 
+
+.html, .htm, .php, and .html.php
+: html file. 
+: considered as html file containing php code.
+
+All of the files are treated like a PHP scripts, so PHP code can be embedded within.
+
+
+###Chain and Dispatch
+
+The basic idea about dispatching controller, or resource object is 
+borrowed (i.e. copied) from BEAR.Sunday. I found it so intuitive, 
+simple, and easy to implement, I could not use any other dispatcher 
+like Symfony. 
+
+Unfortunately, BEAR.Sunday was still under active development when 
+I started WScore projects, I ended up writing some code to demo and 
+test the framework. This version of dispatcher is rather simple 
+(i.e. surfacial) copy of BEAR.Sunday. But has a tiny improvement on 
+having hirarchical dispatcher using Chain of Reponsibility (CoR) pattern. 
+
+Modules
+: All of chain and dispatch are called modules. 
+: Modules can be hirarchically constructed. 
+
+WebApp Module
+: WebApp is a module with extra methods for front-end of web application. 
+: Based on AppChain Module. 
+
+AppChain Module
+: Dispatches a list of modules using CoR pattern. 
+
+AppLoader Module
+: Dispatches Page object based on pathinfo.
+: This module contains Page object (i.e. controller) and View file (templates).
+
+
+###license
+
+[GPL version 2](LICENSE).
+
+
+###patents
+
+The Cena-DTA is patented in Japan. 
--- documents/templates/inspired.md.php
+++ documents/templates/inspired.md.php
@@ -7,34 +7,27 @@ Although I believe I never copied codes fom any of them but
 I looked at their code and design whenever I had difficult 
 time writing codes. 
 
-####[BEAR.Sunday](https://github.com/koriym/BEAR.Sunday)
-
-I learned everything about OOP from BEAR.Sunday.
-
-It is the resource oriented framework, the future of PHP framework.
+[BEAR.Sunday](https://github.com/koriym/BEAR.Sunday)
+: I learned everything about OOP from BEAR.Sunday.
+: It is the resource oriented framework, the future of PHP framework.
 
 
-####[Aura Framework](https://github.com/auraphp/Aura.Framework)
-
-I believe it has the most beautiful and concentrated codes among popular 
-PHP framework I have looked at. (Laravel maybe another one). 
-
-Whenever I had to look up for some implementations and designs, 
-I looked into Aura framework.
+[Aura Framework](https://github.com/auraphp/Aura.Framework)
+: I believe it has the most beautiful and concentrated codes among popular 
+  PHP framework I have looked at. (Laravel maybe another one). 
+: Whenever I had to look up for some implementations and designs, 
+  I looked into Aura framework.
 
 
-####[Symfony](http://symfony.com/)
+[Symfony](http://symfony.com/)
+: It is becoming the de-fact standard framework for PHP... at least it is 
+  a leading locomotive for more OO design and standarization in PHP world. 
 
-It is becoming the de-fact standard framework for PHP... at least it is 
-a leading locomotive for more OO design and standarization in PHP world. 
 
-
-####[Perfect PHP](http://www.amazon.co.jp/dp/4774144371)
-
-It is a book about PHP programming published in November 2010 in Japan. 
-
-It had a simple but good working sample for routing, parsing http requests 
-(i.e. $_SERVER), etc. to kick-start writing a PHP framework. 
+[Perfect PHP](http://www.amazon.co.jp/dp/4774144371)
+: It is a book about PHP programming published in November 2010 in Japan. 
+: It had a simple but good working sample for routing, parsing http requests 
+  (i.e. $_SERVER), etc. to kick-start writing a PHP framework. 
 
 
 a little bit history
