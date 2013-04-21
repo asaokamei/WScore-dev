a8583bf393807f50083f453fec30629022b5f4c5
Asao <kamei@workspot.jp>
2013-04-22 06:51:45 +0900

    added tabs style global menu at the top. but not working good.

--- documents/layout.php
+++ documents/layout.php
@@ -1,6 +1,21 @@
 <?php
-/** @var $this \WScore\Template\TemplateInterface */
-//$this->setDefault( 'basePath', '/WSdev')
+
+/** @var $this \Demo\Classes\Template */
+$baseUrl = $this->get( 'baseUrl' );
+$appRoot = $this->get( 'appRoot' );
+$appRoot = substr( $appRoot, 1 ); // remove first slash... 
+
+// build menus.
+$menu = array(
+    array( 'title' => 'Top',      'icon' => 'home',    'url' => $baseUrl, ),
+    array( 'title' => 'Password', 'icon' => 'home',    'url' => $baseUrl.'pwd/index',  ),
+    array( 'title' => 'Tasks',    'icon' => 'edit',    'url' => $baseUrl.'tasks/', ),
+    array( 'title' => 'Contacts', 'icon' => 'hand-up', 'url' => $baseUrl.'contacts/', ),
+    array( 'title' => 'About',    'icon' => 'file',    'url' => $baseUrl.'templates/index',  ),
+);
+$menu = $this->score->setMenu( $menu, $baseUrl.$appRoot );
+$this->menu->setMenu( $menu )->setTags( 'tabs' );
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -13,11 +28,17 @@
 </head>
 <body>
 <!-- HtmlTest: Needle=documents/layout -->
+<style type="text/css">
+    div#mainMenu > ul { clear: both; float: right;}
+</style>
 <div class="container-narrow">
     <div class="masthead">
         <h3 class="muted"><?php if( $this->HomePage ) { ?>WScore Public Demo<?php } else { echo "<a href=\"{$this->baseUrl}index.php\">WScore Public Demo</a>"; } ?></h3>
+        <div id="mainMenu">
+            <?php echo $this->menu->draw(); ?>
+        </div>
+        <div style="clear:both;"></div>
     </div>
-    <hr>
     <?php echo $this->get( 'content' ); ?>
     <footer class="footer">
         <hr>
