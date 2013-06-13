fb37a0dfa30284dade3fd5adad2e0c2e07178b08
Asao <kamei@workspot.jp>
2013-06-13 21:19:42 +0900

    try to write a script to build class-preloader cache file.

---
+++ app/config/ClassePreloader.php
@@ -0,0 +1,13 @@
+<?php
+/*
+ * ClassPreloader
+ * 
+ * build bootstrap.cache.php file that compiles all WScore's PHP files
+ * into one file for quick loading classes. 
+ * 
+ * run this script whenever a framework class is modified. 
+ * 
+ * Usage:
+ * to-be-written
+ * 
+ */
